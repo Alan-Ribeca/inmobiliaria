3ace5e24c8eb47250263aa945c92c553324b0975
Alan Ribeca <alanribeca2003@gmail.com>
2024-09-04 19:20:45 -0300

    creando y dandole los estilos a los botones de buscar por filtro

--- inmoReact/src/components/propiedades/filtro/Filtro.jsx
+++ inmoReact/src/components/propiedades/filtro/Filtro.jsx
@@ -1,5 +1,13 @@
 import "./filtro.scss";
+import { useState } from "react";
+
 export const Filtro = () => {
+  const [abrir, setAbrir] = useState();
+  const handleClick = (tipo) => {
+    console.log(tipo);
+    setAbrir((prevAbrir) => (prevAbrir === "activo" ? null : "activo"));
+  };
+
   return (
     <>
       <section className="filtros">
@@ -8,8 +16,11 @@ export const Filtro = () => {
           placeholder="¿Donde quieres vivir?"
           className="inputFiltro"
         />
-        <div className="divElegir">
-          <button className="abrir">
+        <div
+          className={`divElegir ${abrir === "activo" ? "abierto" : ""}`}
+          onClick={() => handleClick("activo")}
+        >
+          <button className="abrir propiedad">
             Tipo de propiedad{" "}
             <span className="flechaAbrir">
               <svg
@@ -40,10 +51,6 @@ export const Filtro = () => {
             </label>
             <label className="seleccionado">
               <input type="checkbox" className="check" />
-              Piso
-            </label>
-            <label>
-              <input type="checkbox" className="check" />
               Oficina
             </label>
             <label className="seleccionado">
@@ -56,15 +63,77 @@ export const Filtro = () => {
             </label>
             <label className="seleccionado">
               <input type="checkbox" className="check" />
-              Rancho
-            </label>
-            <label className="seleccionado">
-              <input type="checkbox" className="check" />
               Lote
             </label>
             <label className="seleccionado">
               <input type="checkbox" className="check" />
               Casa en condominio
+            </label>
+          </div>
+        </div>
+
+        <div className="divElegir">
+          <button className="abrir">
+            Alquiler{" "}
+            <span className="flechaAbrir">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="svgFlechaBtn"
+                viewBox="0 0 16 16"
+              >
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+              </svg>
+            </span>{" "}
+          </button>
+
+          <div className="divOculto">
+            <label className="seleccionado">
+              <input type="checkbox" className="check" />
+              Comprar
+            </label>
+            <label className="seleccionado">
+              <input type="checkbox" className="check" />
+              Alquiler
+            </label>
+            <label className="seleccionado">
+              <input type="checkbox" className="check" />
+              Alquiler temporanio
+            </label>
+          </div>
+        </div>
+
+        <div className="divElegir">
+          <button className="abrir ">
+            Precio{" "}
+            <span className="flechaAbrir">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="svgFlechaBtn"
+                viewBox="0 0 16 16"
+              >
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+              </svg>
+            </span>{" "}
+          </button>
+
+          <div className="divOculto">
+            <label className="seleccionado">
+              <input type="checkbox" className="check" />
+              Comprar
+            </label>
+            <label className="seleccionado">
+              <input type="checkbox" className="check" />
+              Alquiler
+            </label>
+            <label className="seleccionado">
+              <input type="checkbox" className="check" />
+              Alquiler temporanio
             </label>
           </div>
         </div>
