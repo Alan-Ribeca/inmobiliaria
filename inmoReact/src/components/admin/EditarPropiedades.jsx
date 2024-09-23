35e23c64507b46163dcca56ea359ae3aa2b134fa
Alan Ribeca <alanribeca2003@gmail.com>
2024-09-23 06:48:42 -0300

    responsi de editar y eliminar prop listo

--- inmoReact/src/components/admin/EditarPropiedades.jsx
+++ inmoReact/src/components/admin/EditarPropiedades.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Propiedad } from "../propiedades/Propiedad";
 import datosAxios from "../../config/axios";
+import './editarPropiedades.scss'
 
 export const EditarPropiedades = () => {
   const [propiedades, setPropiedades] = useState([]);
