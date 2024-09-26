b6dafc33eeb5d4c09358908afd85104dd6787625
Alan Ribeca <alanribeca2003@gmail.com>
2024-09-26 09:13:19 -0300

    agregando los datos en base de datos en la nube

--- apiinmo/index.js
+++ apiinmo/index.js
@@ -3,6 +3,7 @@ const routes = require("./routes");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
+require("dotenv").config({ path: "variables.env" });
 
 // crear el servidor
 const app = express();
@@ -13,7 +14,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // conectar mongo a la bd
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/apiinmo");
+mongoose.connect(process.env.DB_URL);
+
+//definir un dominio para recibir peticiones
+const whiteList = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    const existe = whiteList.some((dominio) => dominio === origin);
+    if (existe) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"), false);
+    }
+  },
+};
 
 //habilitar cors
 const cors = require("cors");
@@ -22,10 +37,16 @@ const cors = require("cors");
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //habilitar cors
-app.use(cors());
+app.use(cors(corsOptions));
 
 // rutas de la app
 app.use("/", routes());
 
 // puerto
-app.listen(2000);
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 2000;
+
+//iniciar app
+app.listen(port, host, () => {
+  console.log("El servidor esta funcionando");
+});
